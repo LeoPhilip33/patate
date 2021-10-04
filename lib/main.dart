455011b30c45fefd3f73454846af8a19a17cdd01
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2021-10-04 16:35:44 +0200

    :sparkles: wouhou

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ynov_immo/constants.dart';
-import 'package:ynov_immo/pages/home/home-screen.dart';
-
+// import 'package:ynov_immo/pages/home/home-screen.dart';
+import 'package:ynov_immo/pages/vente/vente-screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +9,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ynov Immo',
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
           bodyText2: TextStyle(color: ksecondaryColor),
         ),
       ),
-      home: HomeScreen(),
+      home: VenteScreen(),
     );
   }
 }
---
+++ lib/pages/vente/components/body.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Vente extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, title: 'aaaaa');
+  }
+}
