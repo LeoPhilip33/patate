0d3b720466a83e2191497353bb95fec24c2b84cc
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2021-10-04 16:51:57 +0100

    starfoulah

--- lib/pages/vente/components/body.dart
+++ lib/pages/vente/components/body.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class Vente extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, title: 'aaaaa');
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: const Center(
+          child: TextField(
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), hintText: 'Enter a search term'),
+          ),
+        ),
+      ),
+    );
   }
 }
