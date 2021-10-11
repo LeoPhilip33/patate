de1760672dea0cdcf3c8be9010418925111fbf27
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2021-10-11 11:59:54 +0100

    code plus propre

--- lib/pages/vente/components/body.dart
+++ lib/pages/vente/components/body.dart
@@ -7,7 +7,7 @@ class Vente extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: ListView(
         padding: const EdgeInsets.all(8),
-        children: <Widget>[
+        children: [
           Container(
               child: const Center(
             child: Text('Mettre en vente',
@@ -17,51 +17,62 @@ class Vente extends StatelessWidget {
                   fontSize: 25,
                 )),
           )),
-          Container(
-              child: Padding(
-                  padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
-                  child: Text('Phrase d\'accroche',
-                      style: TextStyle(
-                        color: Colors.black,
-                        decoration: TextDecoration.none,
-                        fontSize: 14,
-                      )))),
-          Container(
-              child: TextField(
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), hintText: 'Enter a search term'),
-          )),
-          Container(
-              child: Padding(
-                  padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
-                  child: Text('Description',
-                      style: TextStyle(
-                        color: Colors.black,
-                        decoration: TextDecoration.none,
-                        fontSize: 14,
-                      )))),
-          Container(
-              child: TextField(
-            minLines: 7,
-            maxLines: 8,
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), hintText: 'Description du bien'),
-          )),
-          Container(
-              child: Padding(
-                  padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
-                  child: Text('Surface',
-                      style: TextStyle(
-                        color: Colors.black,
-                        decoration: TextDecoration.none,
-                        fontSize: 14,
-                      )))),
-          Container(
-              child: TextField(
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), hintText: 'Surface en m²'),
-          )),
-          Container(),
+          Column(
+            children: [
+              // Phrase d'accroche
+              const Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                      padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
+                      child: Text('Phrase d\'accroche',
+                          style: TextStyle(
+                            color: Colors.black,
+                            decoration: TextDecoration.none,
+                            fontSize: 14,
+                          )))),
+              const TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter a search term')),
+              // FIN Phrase d'accroche
+
+              // Description
+              const Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                      padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
+                      child: Text('Description',
+                          style: TextStyle(
+                            color: Colors.black,
+                            decoration: TextDecoration.none,
+                            fontSize: 14,
+                          )))),
+              const TextField(
+                minLines: 7,
+                maxLines: 8,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Description du bien'),
+              ),
+              // FIN Description
+
+              // Surface
+              const Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                      padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
+                      child: Text('Surface',
+                          style: TextStyle(
+                            color: Colors.black,
+                            decoration: TextDecoration.none,
+                            fontSize: 14,
+                          )))),
+              const TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Surface en m²')),
+              // FIN Surface
+            ],
+          ),
         ],
       ),
     );
