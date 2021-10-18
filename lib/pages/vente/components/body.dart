071ffadc8f581935af2612547a54d9e47a267de6
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2021-10-18 11:46:46 +0100

    save boulot zejfozjefojnzef

--- lib/pages/vente/components/body.dart
+++ lib/pages/vente/components/body.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
 
-class Vente extends StatelessWidget {
+// ignore: must_be_immutable
+class Vente extends StatefulWidget {
+  @override
+  _InputChip createState() => _InputChip();
+}
+
+class _InputChip extends State<Vente> {
+  List classEnergie = ["A", "B", "C", "D", "E"];
+
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+  String _selectedClassEnergie;
+
+  bool isSelectedJardin = false;
+  bool isSelectedPierresApparentes = false;
+  bool isSelectedCarreauDeCiment = false;
+
+  void initState() {
+    _dropDownMenuItems = buildAndGetDropDownMenuItems(classEnergie);
+    _selectedClassEnergie = _dropDownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<String>> buildAndGetDropDownMenuItems(List energies) {
+    List<DropdownMenuItem<String>> items = new List();
+    for (String energie in energies) {
+      items.add(new DropdownMenuItem(value: energie, child: new Text(energie)));
+    }
+    return items;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -72,6 +101,23 @@ class Vente extends StatelessWidget {
               ),
               // FIN Description
 
+              // Images
+              const Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                      padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
+                      child: Text('Images',
+                          style: TextStyle(
+                            color: Colors.black,
+                            decoration: TextDecoration.none,
+                            fontSize: 14,
+                          )))),
+              const Align(
+                alignment: Alignment.topLeft,
+              ),
+
+              // FIN Images
+
               // Surface
               const Align(
                   alignment: Alignment.topLeft,
@@ -99,63 +145,121 @@ class Vente extends StatelessWidget {
                             decoration: TextDecoration.none,
                             fontSize: 14,
                           )))),
-              Wrap(
-                spacing: 10.0,
-                runSpacing: 20.0,
-                children: [
-                  Container(
-                    child: Text('Jardin :',
-                        style: TextStyle(
-                          color: Colors.black,
-                          decoration: TextDecoration.none,
-                          fontSize: 14,
+              Align(
+                alignment: Alignment.topLeft,
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  children: [
+                    Container(
+                        alignment: Alignment.topLeft,
+                        child: InputChip(
+                          avatar: CircleAvatar(
+                            child: Text(''),
+                          ),
+                          label: Text('Jardin'),
+                          backgroundColor: Colors.grey,
+                          selected: isSelectedJardin,
+                          onSelected: (bool value) {
+                            setState(() {
+                              isSelectedJardin = value;
+                            });
+                          },
+                          selectedColor: Colors.green,
                         )),
-                  ),
-                  Container(
-                    child: Text('Pierres Apparentes :',
-                        style: TextStyle(
-                          color: Colors.black,
-                          decoration: TextDecoration.none,
-                          fontSize: 14,
+                    Container(
+                        alignment: Alignment.topLeft,
+                        child: InputChip(
+                          avatar: CircleAvatar(
+                            child: Text(''),
+                          ),
+                          label: Text('Pierres apparentes'),
+                          backgroundColor: Colors.grey,
+                          selected: isSelectedPierresApparentes,
+                          onSelected: (bool value) {
+                            setState(() {
+                              isSelectedPierresApparentes = value;
+                            });
+                          },
+                          selectedColor: Colors.green,
                         )),
-                  ),
-                  Container(
-                    child: Text('Carreaux de ciment :',
-                        style: TextStyle(
-                          color: Colors.black,
-                          decoration: TextDecoration.none,
-                          fontSize: 14,
+                    Container(
+                        alignment: Alignment.topLeft,
+                        child: InputChip(
+                          avatar: CircleAvatar(
+                            child: Text(''),
+                          ),
+                          label: Text('Carreaux de ciment'),
+                          backgroundColor: Colors.grey,
+                          selected: isSelectedCarreauDeCiment,
+                          onSelected: (bool value) {
+                            setState(() {
+                              isSelectedCarreauDeCiment = value;
+                            });
+                          },
+                          selectedColor: Colors.green,
                         )),
-                  )
-                ],
+                  ],
+                ),
               ),
-              Wrap(
-                spacing: 10.0,
-                runSpacing: 20.0,
-                children: [
-                  Container(
-                    child: Text('Classe Energie :',
-                        style: TextStyle(
-                          color: Colors.black,
-                          decoration: TextDecoration.none,
-                          fontSize: 14,
-                        )),
-                  ),
-                  Container(
-                    child: Text('Classe GES :',
-                        style: TextStyle(
-                          color: Colors.black,
-                          decoration: TextDecoration.none,
-                          fontSize: 14,
-                        )),
-                  ),
-                ],
-              ),
+
+              Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                      padding: EdgeInsets.only(bottom: 10.0, top: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(right: 10.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text('Classe Energie :',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      decoration: TextDecoration.none,
+                                      fontSize: 14,
+                                    )),
+                                DropdownButton(
+                                  value: _selectedClassEnergie,
+                                  items: _dropDownMenuItems,
+                                  onChanged: changedDropDownItem,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 10.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text('Classe GES :',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      decoration: TextDecoration.none,
+                                      fontSize: 14,
+                                    )),
+                                DropdownButton(
+                                  value: _selectedClassEnergie,
+                                  items: _dropDownMenuItems,
+                                  onChanged: changedDropDownItem,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ))),
               // FIN Critères
             ],
           ),
         ],
       ),
     );
+  }
+
+  void changedDropDownItem(String selectedFruit) {
+    setState(() {
+      _selectedClassEnergie = selectedFruit;
+    });
   }
 }
